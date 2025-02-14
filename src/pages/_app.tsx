82fe2e737d08755215bc26197ada85b1df4528cb
Joao Medeiros <jv.medeiros.gallina@gmail.com>
2025-02-14 18:26:03 -0300

    fix: use next image component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
 
+import Image from "next/image";
 import logoImg from '../assets/logo.svg'
+
 import { Container, Header } from "../styles/pages/styles";
 
 globalStyles();
@@ -10,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
       <Component {...pageProps} />
     </Container>

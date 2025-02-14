b317479c2d73fe350d11b3fef891c5132d7aa416
João Medeiros <jv.medeiros.gallina@gmail.com>
2025-02-13 21:15:53 -0300

    feat: impl global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
 
+globalStyles();
+
 export default function App({ Component, pageProps }: AppProps) {
-  globalStyles();
 
   return <Component {...pageProps} />
 }
